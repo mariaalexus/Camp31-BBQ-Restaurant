79215eaad52872f88d4daa410de263623c8a0252
mariaalexus <mariaalexusramos@gmail.com>
2022-03-23 20:59:06 -0700

    adding menus

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ function myFunction() {
     confirm("Thanks for contacting us!");
   }
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
