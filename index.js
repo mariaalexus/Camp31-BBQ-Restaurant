22d6240b131177f42f20c93ca2c59eb677507b11
mariaalexus <mariaalexusramos@gmail.com>
2022-03-23 22:28:47 -0700

    navbar

--- index.js
+++ index.js
@@ -2,17 +2,3 @@ function myFunction() {
     confirm("Thanks for contacting us!");
   }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
